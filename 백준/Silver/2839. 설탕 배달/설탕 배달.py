5d519bc60e18b4de1f110b7e30e57033d7b4e981
Ryu Hyundam <101377487+yhd0210@users.noreply.github.com>
2023-04-19 17:30:03 +0900

    [Silver IV] Title: 설탕 배달, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.py
@@ -0,0 +1,12 @@
+n = int(input())
+result = 0
+
+while n >= 0 :
+    if n % 5 == 0 :
+        result += (n // 5)
+        print(result)
+        break
+    n -= 3
+    result += 1
+else :
+    print(-1)

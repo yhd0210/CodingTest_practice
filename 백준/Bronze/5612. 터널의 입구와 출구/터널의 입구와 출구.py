1e3ebb449638b161110ad760a9faa81029e98503
Ryu Hyundam <101377487+yhd0210@users.noreply.github.com>
2023-06-18 14:20:08 +0900

    [Bronze III] Title: 터널의 입구와 출구, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/5612. 터널의 입구와 출구/터널의 입구와 출구.py
@@ -0,0 +1,10 @@
+import sys
+n=int(sys.stdin.readline())
+tmp=[]
+tmp.append(int(sys.stdin.readline()))
+for i in range(n):
+    a,b=map(int,sys.stdin.readline().split())
+    tmp.append( tmp[i]+a-b)
+for i in range(n+1):
+    if tmp[i]<0: print(0); exit()
+print(max(tmp))

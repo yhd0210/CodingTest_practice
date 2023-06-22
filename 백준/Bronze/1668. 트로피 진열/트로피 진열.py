30cf5a4a6fdb558c769da78879bc005da93ac379
Ryu Hyundam <101377487+yhd0210@users.noreply.github.com>
2023-06-22 23:05:23 +0900

    [Bronze II] Title: 트로피 진열, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1668. 트로피 진열/트로피 진열.py
+++ 백준/Bronze/1668. 트로피 진열/트로피 진열.py
@@ -1,12 +1,12 @@
 N = int(input())
-li = [int(input()) for _ in range(N)]
+ls = [int(input()) for _ in range(N)]
 left_cnt = right_cnt = 0
 left_max = right_max = 0
-for n in li:
+for n in ls:
     if n > left_max:
         left_max = n
         left_cnt += 1
-for n in li[::-1]:
+for n in ls[::-1]:
     if n > right_max:
         right_max = n
         right_cnt += 1

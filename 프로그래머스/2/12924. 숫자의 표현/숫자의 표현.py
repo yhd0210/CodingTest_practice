dab9868dfc0c83a43d29daa431b2e0ae94595266
Ryu Hyundam <101377487+yhd0210@users.noreply.github.com>
2025-01-07 10:17:49 +0900

    [level 2] Title: 숫자의 표현, Time: 5.97 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.py
@@ -1,14 +1,2 @@
-def solution(num):
-    answer = 0
-    for x in range(1,num+1):
-        sum = 0
-        for y in range(x, num+1):
-            sum += y
-            if sum == num:
-                answer += 1
-                break
-            elif sum > num:
-                break
-
-    return answer
- 
+def solution(n):
+    return len([i for i in range(1, n+1, 2) if n % i == 0])

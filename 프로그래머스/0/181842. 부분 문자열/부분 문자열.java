eee8ed0bbd5f0e07fe32c52b60e2304c86a835a7
Ryu Hyundam <101377487+yhd0210@users.noreply.github.com>
2025-01-14 12:51:49 +0900

    [level 0] Title: 부분 문자열, Time: 0.05 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
+++ 프로그래머스/0/181842. 부분 문자열/부분 문자열.java
@@ -1,5 +1,16 @@
 class Solution {
     public int solution(String str1, String str2) {
-        return str2.contains(str1) ? 1 : 0;
+        return isSubstring(str1, str2) ? 1 : 0;
+    }
+    public static boolean isSubstring(String str1, String str2) {
+        for (int i = 0; i <= str2.length() - str1.length(); i++) {
+            if (str2.substring(i, i + str1.length()).equals(str1)) {
+                return true;
+            }
+        }
+        return false; 
     }
 }
+
+// 7 - 3
+// 0000000

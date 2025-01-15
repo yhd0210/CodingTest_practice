9cb07c338ffdcedff4e2fb4bd97fcfd6d743c329
Ryu Hyundam <101377487+yhd0210@users.noreply.github.com>
2025-01-15 13:02:06 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.07 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -5,11 +5,8 @@ class Solution {
         if (number == 1) return 0;
         while (number != 1) {
             if (answer >= 500) return -1;
-            if (number % 2 == 0) {
-                number /= 2;
-            } else {
-                number = number * 3 + 1;
-            }
+            if (number % 2 == 0) number /= 2;
+            else number = number * 3 + 1;
             answer++;
         }
         return answer;
